97da97deae952681149b17870e06b4a74ebe10e6
Jon Mischo <jon@polymathicon.com>
2026-04-30 12:52:57 -0700

    Restrict run-id actor fallback

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -13,7 +13,7 @@ function hashToken(token: string) {
   return createHash("sha256").update(token).digest("hex");
 }
 
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 interface ActorMiddlewareOptions {
   deploymentMode: DeploymentMode;
